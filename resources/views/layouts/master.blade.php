0374d168397eb97a4908603dd84358ba2d252cbd
Omer Odabasoglu <omerpasha34@gmail.com>
2017-05-11 12:50:12 -0700

    Testing the URLs

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -24,9 +24,9 @@
 
             <ul class="nav nav-tabs">
                 @if(Auth::check())
-                    <li class="{{ Request::is('/') ? "active" : '' }}"><a class="{{ Request::is('/') ? "current" : '' }}" href="./" data-toggle="tab" aria-expanded="true">HOME</a></li>
-                    <li class="{{ Request::is('sports') ? "active" : '' }}"><a class="{{ Request::is('sports') ? "current" : '' }}" href="./sports" data-toggle="tab" aria-expanded="true">ADD A NEW COLLEGE</a></li>
-                    <li class="{{ Request::is('print') ? "active" : '' }}"><a class="{{ Request::is('print') ? "current" : '' }}" href="./print" data-toggle="tab" aria-expanded="true">PRINT LIST</a></li>
+                    <li class="{{ Request::is('/') ? "active" : '' }}"><a class="{{ Request::is('/') ? "current" : '' }}" href="/" data-toggle="tab" aria-expanded="true">HOME</a></li>
+                    <li class="{{ Request::is('sports') ? "active" : '' }}"><a class="{{ Request::is('sports') ? "current" : '' }}" href="/sports" data-toggle="tab" aria-expanded="true">ADD A NEW COLLEGE</a></li>
+                    <li class="{{ Request::is('print') ? "active" : '' }}"><a class="{{ Request::is('print') ? "current" : '' }}" href="/print" data-toggle="tab" aria-expanded="true">PRINT LIST</a></li>
                     <li>
                         <form method='POST' id="logout "action='./logout'>
                             {{csrf_field()}}
